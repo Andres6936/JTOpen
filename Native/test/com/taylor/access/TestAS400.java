1e31a5c9c5240d66ad74371f6a8cf1dc6e97cc63
Adan <adan@maildrop.cc>
2021-08-17 17:41:57 -0500

    Added Test

---
+++ Native/test/com/taylor/access/TestAS400.java
@@ -0,0 +1,30 @@
+package com.taylor.access;
+
+import com.ibm.as400.access.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class TestAS400 {
+    @Test
+    public void testConnection() throws AS400SecurityException, IOException, ObjectDoesNotExistException, InterruptedException, ErrorCompletingRequestException {
+        AS400 power = new AS400("192.168.1.3", "JBURIT", "HDGTDVI5");
+        power.connectService(AS400.FILE);
+        power.connectService(AS400.COMMAND);
+        power.connectService(AS400.DATABASE);
+        Assert.assertTrue(power.isConnected());
+
+        IFSFile file = new IFSFile(power, "/archivo.csv");
+        BufferedReader reader = new BufferedReader(new IFSFileReader(file));
+        // Read the first line of the file, converting characters.
+        String line1 = reader.readLine();
+        // Display the String that was read.
+        System.out.println(line1);
+        // Close the reader.
+        reader.close();
+
+        power.disconnectAllServices();
+    }
+}

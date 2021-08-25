f2753152ee8eb6c7fb788ff912df33f07f9c1df9
Adan <adan@maildrop.cc>
2021-08-25 14:58:36 -0500

    Implemented methods

--- Native/src/com/ibm/as400/access/SQLDecFloat16.java
+++ Native/src/com/ibm/as400/access/SQLDecFloat16.java
@@ -15,18 +15,14 @@ package com.ibm.as400.access;
 
 import java.io.InputStream;
 import java.math.BigDecimal;
-import java.sql.Blob;
-import java.sql.Date;
-/* ifdef JDBC40 
+import java.sql.*;
+/* ifdef JDBC40
 import java.sql.NClob;
 import java.sql.RowId;
 endif */
-import java.sql.SQLException;
-/* ifdef JDBC40 
+/* ifdef JDBC40
 import java.sql.SQLXML;
 endif */
-import java.sql.Time;
-import java.sql.Timestamp;
 import java.util.Calendar;
 
 //@DFA new class
@@ -648,21 +644,21 @@ final class SQLDecFloat16 extends SQLDataBase {
     }
 
     //@pda jdbc40
-    /* ifdef JDBC40 
+
     public RowId getRowId() throws SQLException
     {
         JDError.throwSQLException(this, JDError.EXC_DATA_TYPE_MISMATCH);
         return null;
     }
-   endif */
+
     //@pda jdbc40
-    /* ifdef JDBC40 
+
     public SQLXML getSQLXML() throws SQLException
     {
         JDError.throwSQLException(this, JDError.EXC_DATA_TYPE_MISMATCH);
         return null;
     }
-    endif */
+
     // @array
 
     public void saveValue() {
